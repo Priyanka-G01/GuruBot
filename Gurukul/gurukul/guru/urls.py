41dc6dfae3d506e2f9000849176befc8d11d9a2c
Priyanka-G01 <67776590+Priyanka-G01@users.noreply.github.com>
2022-01-23 22:54:07 +0530

    Delete Gurukul directory

--- Gurukul/gurukul/guru/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path,include
-from . import views
-
-urlpatterns = [
-    path('',views.home ),
-    path('get/',views.get_bot_response),
-]
